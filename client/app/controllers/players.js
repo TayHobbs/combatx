83586d2a08e615f689eec3ef59cc9a72739482dd
Taylor Hobbs <taylor@feemon.com>
2015-05-07 21:20:26 -0500

    Refactor: get rid of unneccessary component

--- client/app/controllers/players.js
+++ client/app/controllers/players.js
@@ -3,6 +3,23 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   battle: Ember.inject.service(),
 
+  setup: function () {
+    io.socket.get('/api/v1/player');
+    io.socket.on('connect', () => {
+      Ember.Logger.debug('combatx component is listening for socket.io events');
+    });
+
+    io.socket.on('player', (message) => {
+      if (message.verb === 'created') {
+        this.get('model').pushObject(message.data);
+      }
+      if (message.verb === 'updated') {
+        this.set('model', Ember.A([message.data]));
+      }
+    });
+
+  }.on('init'),
+
   actions: {
     addPlayer() {
       let player = { name: this.get('name'), health: 100 };
