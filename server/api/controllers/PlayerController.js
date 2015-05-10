0e4e3e46e0dac16e46083440146a49ec4ab0bf19
Taylor Hobbs <taylor@feemon.com>
2015-05-10 17:18:23 -0500

    Bug Fix: Actually only get players from the last 24 hours

--- server/api/controllers/PlayerController.js
+++ server/api/controllers/PlayerController.js
@@ -2,7 +2,8 @@
 module.exports = {
 
   leaderboard: function(req, res) {
-    Player.find({ createdAt: { '>': new Date().setDate(new Date().getDate()-1) }}, function(err, players) {
+    var yesterday = new Date().setDate(new Date().getDate() - 1);
+    Player.find({ createdAt: { '>': new Date(yesterday) }}, function(err, players) {
       return res.json(players);
     });
   },
