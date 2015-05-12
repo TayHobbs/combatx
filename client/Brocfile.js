5b05c643d853e557d2de5170d8152018f7fc5364
Taylor Hobbs <taylor@feemon.com>
2015-05-11 20:42:13 -0500

    Dependency: add bootstrap

--- client/Brocfile.js
+++ client/Brocfile.js
@@ -4,6 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
+app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
 app.import('vendor/sails.io.js');
 
 module.exports = app.toTree();
